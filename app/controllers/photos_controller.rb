26794ed6750af6448ef2d7589eef906dff0e11c6
woonchee <king.828@hotmail.com>
2016-09-10 08:01:33 +0000

    Created a rails application and added status update

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,35 @@
+class PhotosController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+     def index
+        @photos = Photo.order(created_at: :desc)
+     end
+
+     def new
+        @photo=Photo.new
+     end
+
+  def create
+     @photo = current_user.photos.new(photo_params)
+    if @photo.save
+      redirect_to photo_path(@photo)
+      else
+      render:new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+  def destroy
+        @photo = Photo.find(params[:id])
+        @photo.destroy
+
+        redirect_to photos_path
+   end
+
+
+  private
+  def photo_params
+    params.require(:photo).permit(:file, :caption)
+  end
+end
